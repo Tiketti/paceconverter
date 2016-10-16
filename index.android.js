1cb04e4cf084708ab4ff779d99d21d7eb535f738
Perttu <perttu@gmail.com>
2016-10-16 22:04:25 +0300

    Add basic inputs, styles, lint rules

--- index.android.js
+++ index.android.js
@@ -1,43 +1,79 @@
+'use strict';
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput
 } from 'react-native';
 
 class paceconverter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '0:00' };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Pace Converter
-        </Text>
-        <Text style={styles.instructions}>
-          Convert pace from minutes per mile to {'\n'}
-          minutes per kilometer and vice versa.
-        </Text>
+        <View style={styles.header}>
+          <Text style={styles.welcome}>
+            Pace Converter
+          </Text>
+          <Text style={styles.instructions}>
+            Convert pace from minutes per mile to {'\n'}
+            minutes per kilometer and vice versa.
+          </Text>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({ text })}
+            value={this.state.text} />
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({ text })}
+            value={this.state.text} />
+        </View>
       </View>
     );
   }
 }
 
+const lightColor = '#ffffff';
+const darkColor = '#b00000';
+const inputBg = 'skyblue';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: darkColor, 
+    padding: 10 
+  },
+  inputContainer: {
+    flex: 2,
     alignItems: 'center',
-    backgroundColor: '#a9a9a9',
+    backgroundColor: inputBg,
+  },
+  input: {
+    width: 200,
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 25
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-    color: '#000000',
+    color: lightColor,
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: lightColor,
     marginBottom: 5,
   },
 });
